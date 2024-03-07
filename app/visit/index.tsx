3c06fc9fcb7ac26e6561d1237bda14daadbcbf61
Canariiii <josembravomendez@gmail.com>
2024-03-07 13:50:41 +0000

    visit gradient color#1

--- app/visit/index.tsx
+++ app/visit/index.tsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { SafeAreaView, Text, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import PrimaryButton from "../../components/buttons/primaryButton";
 
-const Visit = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <LinearGradient colors={["#F44E4E", "#737373"]} style={styles.gradient}>
-      </LinearGradient>
-    </SafeAreaView>
-  );
+const NotEligible = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <LinearGradient
+                colors={["#FE4A4A", "#E96C6C"]}
+                start={[1, 0]}
+                end={[0, 0.8]}
+                style={styles.gradient}
+            >
+                <Text style={styles.title}>We’re bloody sorry...</Text>
+                <Text>Because of [insert reason] it looks like you’re not eligible tto donate blood</Text>
+            </LinearGradient>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  gradient: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
+    container: {
+        flex: 1,
+    },
+    gradient: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
 });
 
-export default Visit;
+export default NotEligible;
