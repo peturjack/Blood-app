42d738022a0df9b6e46e76442998bfc6c958a1b1
Canariiii <josembravomendez@gmail.com>
2024-03-08 09:32:11 +0000

    fix some background

--- app/visit/index.tsx
+++ app/visit/index.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import { SafeAreaView, Text, View, StyleSheet, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import PrimaryButton from "../../components/buttons/primaryButton";
+import Gradient from "../../components/colors/gradient";
 
-
-const NotEligible = () => {
+const Visit = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <LinearGradient colors={["#FE4A4A", "#E96C6C"]} start={[1, 0]} end={[0, 0.8]} style={styles.gradient} >
-        <View className="flex h-[100%] items-center">
-          <Image source={require('../../assets/white_drop_2_2.svg')} className="absolute w-[50%] top-[15%] left-[20%]" />
-          <Text className="absolute font-bold text-4xl text-white top-[30%]">Blood Buddy</Text>
-          <View className="flex h-[90%] justify-end items-center">
-            <PrimaryButton route="/" title="Sign In" />
-            <Text className="text-white mt-5">Don't have an account yet? Sign Up!</Text>
-          </View>
+    <Gradient>
+      <View className="flex h-[100%] items-center">
+        <Image source={require('../../assets/white_drop_2_2.svg')} className="absolute w-[50%] top-[15%] left-[20%]" />
+        <Text className="absolute font-bold text-4xl text-white top-[30%]">Blood Buddy</Text>
+        <View className="flex h-[90%] justify-end items-center">
+          <PrimaryButton route="/" title="Sign In" />
+          <Text className="text-white mt-5">Don't have an account yet? Sign Up!</Text>
         </View>
-      </LinearGradient>
-    </SafeAreaView>
+      </View>
+    </Gradient>
   );
 };
 
@@ -37,4 +35,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default NotEligible;
+export default Visit;
