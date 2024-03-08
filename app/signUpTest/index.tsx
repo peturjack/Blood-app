ca16a50fd67cf64c39810e875f46cf9c5e665da1
Canariiii <josembravomendez@gmail.com>
2024-03-08 11:24:14 +0000

    test page started

--- app/signUpTest/index.tsx
+++ app/signUpTest/index.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { Text, View } from "react-native";
 import TestedButtons from "../../components/buttons/letsGetTestedButtons";
 import PrimaryButton from "../../components/buttons/primaryButton";
+import Gradient from "../../components/colors/gradient";
 
 const SignUpTest = () => {
   return (
-    <>
-      <SafeAreaView>
-        <TestedButtons route="/notEligible" title="No" />
-        <Text>This is where your sign up test will take place</Text>
-        <PrimaryButton isPrimary={true} route="/signUpTest" title="Sign In" />
-      </SafeAreaView>
-    </>
+    <Gradient>
+      <View className="flex h-[100%] items-center">
+        <View className="h-[25%] mt-[10%] items-center">
+          <Text className="text-4xl text-bold text-white ">Let’s get tested!</Text>
+          <Text className="mt-10 text-2xl text-white">Take the test below to see if you’re eligible for blood donation</Text>
+        </View>
+        <View className="h-[50%] flex-row">
+          <TestedButtons route="/notEligible" title="No" />
+          <TestedButtons route="/notEligible" title="No" />
+        </View>
+        <PrimaryButton isPrimary={false} route="/visit" title="Finish Test" />
+      </View>
+    </Gradient>
   );
 };
 
