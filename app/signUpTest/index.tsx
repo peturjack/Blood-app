4541c96eaabee772bfa39deb48cd864e077f6f09
Canariiii <josembravomendez@gmail.com>
2024-03-10 20:54:57 +0000

    scroll view

--- app/signUpTest/index.tsx
+++ app/signUpTest/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import TestedButtons from "../../components/buttons/letsGetTestedButtons";
 import PrimaryButton from "../../components/buttons/primaryButton";
 import Gradient from "../../components/colors/gradient";
@@ -7,9 +7,9 @@ import Gradient from "../../components/colors/gradient";
 const SignUpTest = () => {
   return (
     <Gradient>
-      <View className="flex h-[100%] items-center">
-        <View className="h-[22%] mt-[10%] items-center">
-          <Text className="text-4xl font-bold text-white ">Let’s get tested!</Text>
+      <ScrollView contentContainerStyle={{ flex: 1, alignItems: "center" }}>
+        <View className="h-[20%] mt-[10%] items-center">
+          <Text className="text-4xl font-bold text-white">Let’s get tested!</Text>
           <Text className="mt-10 text-2xl text-white">Take the test below to see if you’re eligible for blood donation</Text>
         </View>
         <View className="h-[15%] flex-row justify-between w-[212px]">
@@ -32,8 +32,23 @@ const SignUpTest = () => {
           <TestedButtons title="Yes" />
           <TestedButtons title="No" />
         </View>
+        <View className="h-[15%] flex-row justify-between w-[212px]">
+          <Text className="absolute h-[54px] w-[370px] text-white self-center mb-[60%]">Do you weight more than 50KG?</Text>
+          <TestedButtons title="Yes" />
+          <TestedButtons title="No" />
+        </View>
+        <View className="h-[15%] flex-row justify-between w-[212px]">
+          <Text className="absolute h-[54px] w-[370px] text-white self-center mb-[60%]">Do you weight more than 50KG?</Text>
+          <TestedButtons title="Yes" />
+          <TestedButtons title="No" />
+        </View>
+        <View className="h-[15%] flex-row justify-between w-[212px]">
+          <Text className="absolute h-[54px] w-[370px] text-white self-center mb-[60%]">Do you weight more than 50KG?</Text>
+          <TestedButtons title="Yes" />
+          <TestedButtons title="No" />
+        </View>
         <PrimaryButton isPrimary={false} route="/visit" title="Finish Test" />
-      </View>
+      </ScrollView>
     </Gradient>
   );
 };
