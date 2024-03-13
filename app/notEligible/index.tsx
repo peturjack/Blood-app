0200a66bd714c79b5d7c16d9105f827b5db7fbc6
AdrianArmasRincon <adrianyaredarmasdelanuez@alumno.ieselrincon.es>
2024-03-13 13:13:23 +0000

    user session status and custom message per user

--- app/notEligible/index.tsx
+++ app/notEligible/index.tsx
@@ -5,6 +5,7 @@ import { View, Text, TextInput, Image, Button, Alert } from "react-native";
 import Gradient from "../../components/colors/gradient";
 import { createClient } from '@supabase/supabase-js';
 
+
 const supabaseUrl = 'https://lathnvpabbkjsfejvsmb.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhdGhudnBhYmJranNmZWp2c21iIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwOTY0MTEyMywiZXhwIjoyMDI1MjE3MTIzfQ.b2b2QuL-JDnnx-mU8fioyL9tkC6ScH8mtatqmIVg838';
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -12,35 +13,69 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const NotEligible = () => {
 
   // Waitting to post form errors on db to use commented code:
-    const [userError, setUserError] = useState("");
+  const [userError, setUserError] = useState("");
 
-    useEffect(() => {
-      getUserError();
-      console.log("getUserError: " + getUserError().toString);
-    }, []);
+  useEffect(() => {
+    userStatus();
+    getUserError();
+    console.log("getUserError: " + getUserError().toString);
+  }, []);
 
-    const getUserError = async () => {
-      try {
-        var userId = 1;// Here comes actual user id, you should swap it with a local variable stored while logging in
-        const { data, error } = await supabase
-          .from('userdata')
-          .select('testIssues')
-          .eq('id', userId);
 
-        if (error) {
-          console.error("Error getting user data:", error.message);
+ 
+  const userStatus = async () => {
+    // Test if user is logged in
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') {
+        if (session) {
+          const currentUser = session.user;
+          const userId = currentUser.id;
+          console.log("user id:", userId);
         } else {
-          if (data && data.length > 0) {
-            setUserError(data[0].testIssues);
-          } else {
-            console.error("No errors found");
-          }
+          console.log("Couldn't get user data.");
         }
-      } catch (error) {
-        console.error("Error:", error);
+      } else if (event === 'SIGNED_OUT') {
+        // session closed
+        console.log("User disconnected");
       }
-    };
+    });
+    
+    //get user data
+    const{ data: { user } } = await supabase.auth.getUser();
 
+    // Check if user is already logged in
+    const currentUser = { user }.user?.id;
+  if (currentUser) {
+    const userId = { user }.user?.id;
+    console.log("ID del usuario actual:", userId);
+  } else {
+    console.log("No hay usuario autenticado");
+  }
+  };
+  
+
+  //get user error
+  const getUserError = async () => {
+    try {
+      var userId =  userStatus();// Here comes actual user id, you should swap it with a local variable stored while logging in
+      const { data, error } = await supabase
+        .from('userdata')
+        .select('testIssues')
+        .eq('id', userId);
+
+      if (error) {
+        console.error("Error getting user data:", error.message);
+      } else {
+        if (data && data.length > 0) {
+          setUserError(data[0].testIssues);
+        } else {
+          console.error("No errors found");
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
     <>
@@ -58,7 +93,7 @@ const NotEligible = () => {
               </Text>
               <Link href="../signUpTest/index" className=" mt-10">
                 <Text className="text-white text-[23px] mb-3">Answered wrong on the test?</Text>
-                <br/>
+                <br />
                 <Text className="text-white font-bold text-[25px] border-b-2 border-white mb-3 ml-5">
                   Change your answer
                 </Text>
