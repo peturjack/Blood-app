ccb545b8fc3247f88c90b4bcda68912a843d0480
Canariiii <josembravomendez@gmail.com>
2024-03-14 11:38:05 +0000

    pull problems

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from "react-native";
 export default function HomeLayout() {
   return (
     <>
-      <SafeAreaView className="flex-1 w-[100%] m-auto">
+      <SafeAreaView className="flex-1 w-[100%]">
         <Slot />
       </SafeAreaView>
     </>
