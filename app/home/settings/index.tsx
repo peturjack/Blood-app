bec40e7e344e476f5c5705440278b66b3cd8ad62
Canariiii <josembravomendez@gmail.com>
2024-03-14 18:48:16 +0000

    settings page #1

--- app/home/settings/index.tsx
+++ app/home/settings/index.tsx
@@ -16,17 +16,55 @@ const Settings = () => {
           <Text className="text-white font-bold text-[24px] mt-5">Settings</Text>
         </View>
         <View className="mt-5 h-[231px] w-[362px] border-2 border-[#F44E4E] rounded-xl">
-          <Text>Access</Text>
+          <Text className="font-bold text-[24px] left-10 top-[24px]">Access</Text>
+          <View className="p-5 mt-[24px] pl-10">
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Location</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Camera</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Photos</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+          </View>
         </View>
         <View className="mt-5 h-[231px] w-[362px] border-2 border-[#F44E4E] rounded-xl">
-          <Text>Notifications</Text>
+          <Text className="font-bold text-[24px] left-10 top-[24px]">Notifications</Text>
+          <View className="p-5 mt-[24px] pl-10">
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Allow emails</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Allow texts</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Allow notifications</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+          </View>
         </View>
         <View className="mt-5 h-[231px] w-[362px] border-2 border-[#F44E4E] rounded-xl">
-          <Text>Password</Text>
+          <Text className="font-bold text-[24px] left-10 top-[24px]">Password</Text>
+          <View className="p-5 mt-[24px] pl-10">
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Allow FaceID</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+            <View className="flex-row justify-between">
+              <Text className="text-[20px]">Allow FingerID</Text>
+              <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+            </View>
+          </View>
         </View>
         <View className="mt-5 h-[231px] w-[362px] border-2 border-[#F44E4E] rounded-xl">
-          <Text>User Info</Text>
-          <SettingsButtons onToggle={handleToggle}></SettingsButtons>
+          <Text className="font-bold text-[24px] left-10 top-[24px]">User Info</Text>
+
         </View>
       </View>
     </ScrollView>
---
+++ app/visit/index 2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Visit = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Visit
